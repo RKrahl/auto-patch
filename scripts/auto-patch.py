324642f16a682014314b97489e307568ff2244da
Rolf Krahl <rolf@rotkraut.de>
2020-07-26 14:57:32 +0200

    Add a class Zypper to represent calls to zypper.

--- scripts/auto-patch.py
+++ scripts/auto-patch.py
@@ -7,47 +7,66 @@ import subprocess
 import tempfile
 import sys
 
-_zypper = "/usr/bin/zypper"
 
-def call_zypper(args, stdout=None, retcodes=None):
-    cmd = [_zypper] + args
-    proc = subprocess.run(cmd, stdout=stdout, stderr=subprocess.PIPE,
-                          universal_newlines=True)
-    if proc.returncode != 0 and not (retcodes and proc.returncode in retcodes):
-        proc.check_returncode()
-    return proc.returncode
+class Zypper:
 
-def patch(stdout=None):
-    # patch-check
-    # 0: no patches needed
-    # 100: patches available for installation
-    # 101: security patches available for installation
-    args = ["--quiet", "--non-interactive", "patch-check"]
-    rc = call_zypper(args, stdout=stdout, retcodes={100, 101})
-    if rc == 0:
-        return False
-    while True:
-        # list-patches: add some more diagnostic info to the output
+    _zypper = "/usr/bin/zypper"
+
+    @classmethod
+    def call(cls, args, stdout=None, retcodes=None):
+        cmd = [cls._zypper] + args
+        proc = subprocess.run(cmd, stdout=stdout, stderr=subprocess.PIPE,
+                              universal_newlines=True)
+        if (proc.returncode != 0 and
+            not (retcodes and proc.returncode in retcodes)):
+            proc.check_returncode()
+        return proc.returncode
+
+    @classmethod
+    def patch_check(cls, stdout=None):
+        # patch-check
+        # 0: no patches needed
+        # 100: patches available for installation
+        # 101: security patches available for installation
+        args = ["--quiet", "--non-interactive", "patch-check"]
+        return cls.call(args, stdout=stdout, retcodes={100, 101})
+
+    @classmethod
+    def list_patches(cls, stdout=None):
         args = ["--quiet", "--non-interactive", "list-patches"]
-        rc = call_zypper(args, stdout=stdout)
+        return cls.call(args, stdout=stdout)
+
+    @classmethod
+    def patch(cls, stdout=None):
         # patch: install patches
         # 0: ok
         # 102: successful installation, patch requires reboot
         # 103: successful installation, restart of package manager needed
-        args = ["--quiet", "--non-interactive",
-                "patch", "--skip-interactive"]
-        rc = call_zypper(args, stdout=stdout, retcodes={102, 103})
+        args = ["--quiet", "--non-interactive", "patch", "--skip-interactive"]
+        return cls.call(args, stdout=stdout, retcodes={102, 103})
+
+    @classmethod
+    def ps(cls, stdout=None):
+        args = ["--quiet", "ps"]
+        return cls.call(args, stdout=stdout)
+
+
+def patch(stdout=None):
+    if Zypper.patch_check(stdout=stdout) == 0:
+        return False
+    while True:
+        Zypper.list_patches(stdout=stdout)
+        rc = Zypper.patch(stdout=stdout)
         if rc == 0:
             break
         elif rc == 102:
-            # should normally not happen, as we skipped
-            # interactive patches.
+            # patch requires reboot.
+            # should normally not happen, as we skipped interactive patches.
             break
         elif rc == 103:
+            # restart of package manager needed.
             continue
-    # ps: lists all processes using deleted files
-    args = ["--quiet", "ps"]
-    rc = call_zypper(args, stdout=stdout)
+    Zypper.ps(stdout=stdout)
     return True
 
 if __name__ == "__main__":
