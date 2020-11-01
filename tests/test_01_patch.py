ed7ea39396b7608728e778b742fa180d0272994b
Rolf Krahl <rolf@rotkraut.de>
2020-11-01 17:41:14 +0100

    Add more test cases.

--- tests/test_01_patch.py
+++ tests/test_01_patch.py
@@ -1,3 +1,6 @@
+"""Some standard scenarios applying patches with auto-patch.py.
+"""
+
 import pytest
 
 
@@ -8,8 +11,36 @@ def test_no_patches(tmpdir, auto_patch_caller):
         auto_patch_caller.run()
 
 
+def test_rec_patches(tmpdir, auto_patch_caller):
+    """Some recommended patches available, but no security patches.
+    """
+    with tmpdir.as_cwd():
+        auto_patch_caller.run()
+
+
 def test_sec_patches(tmpdir, auto_patch_caller):
     """Some security patches available.
+    """
+    with tmpdir.as_cwd():
+        auto_patch_caller.run()
+
+
+def test_zypp_patches(tmpdir, auto_patch_caller):
+    """A patches affects the package manager itself, restart patch required.
+    """
+    with tmpdir.as_cwd():
+        auto_patch_caller.run()
+
+
+def test_patch_psproc(tmpdir, auto_patch_caller):
+    """After applying patches, 'zypper ps' reports processes needing restart.
+    """
+    with tmpdir.as_cwd():
+        auto_patch_caller.run()
+
+
+def test_patch_psreboot(tmpdir, auto_patch_caller):
+    """After applying patches, 'zypper ps' reports reboot is needed.
     """
     with tmpdir.as_cwd():
         auto_patch_caller.run()
