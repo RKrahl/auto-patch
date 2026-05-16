47f3eeb6482a77fcfe41ed99274ec64160fa31d5
Rolf Krahl <rolf@rotkraut.de>
2026-05-16 21:05:09 +0200

    Tested with Python 3.15.0b1

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.15",
         "Topic :: System :: Systems Administration",
     ],
     project_urls = dict(
